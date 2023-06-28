14991dae7393ea8cdfea608b731ff950fac7c998
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-06-28 15:20:53 -0300

    feat: sending responses

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const http = require('http')
 
 
 const server = http.createServer((req, res) => {
-  console.log(req)
+  console.log(req.url, req.method, req.headers)
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<html>')
+  res.write('<head><title>Node js Server Page</title></head>')
+  res.write('<body><h1> Hello from my node js server </h1></body>')
+  res.write('</html>')
+  res.end()
 })
 
 server.listen(3000)
