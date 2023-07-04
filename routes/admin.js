9d9dadb2761d7ea194a3f16d8975a079680ed1eb
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-07-04 18:48:09 -0300

    refactor: rootDir path

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const path = require('path')
+const rootDir = require('../utils/path')
 
 const router = express.Router()
 
+
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
 
 // /admin/add-product => POST
