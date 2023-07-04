ccd172a69d85aaf86ec01b77d8899c539ba8545a
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-07-04 15:31:03 -0300

    feat: handling routes with express

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 const port = 3000
 
 const app = express()
 
+app.use(bodyParser.urlencoded({extended: false}))
+
 app.get('/favicon.ico', (req, res) => res.status(204));
 
-app.use((req, res, next) => {
-  console.log('in the middleware', req.url)
-  
-  next() // Allows the request to continue to the next middleware in line
-  
-  
-})
+app.use('/admin', adminRoutes)
+
+app.use(shopRoutes)
 
 app.use((req, res, next) => {
-  console.log('in another middleware', req.url)
-  res.send('<h1>test for echo</h1>')
+  res.status(404).send('<h1>Page not Found...</h1>')
 })
+
+
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
