e1dce23e7d6026f92bf637f968a013547463a446
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-07-04 17:51:57 -0300

    feat: filtering routes and serving html pages

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const bodyParser = require('body-parser')
 
 const adminRoutes = require('./routes/admin')
@@ -17,7 +18,7 @@ app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not Found...</h1>')
+  res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'))
 })
 
 
