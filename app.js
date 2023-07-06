dd108023d0813471c482e322b38abaab3034b047
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-07-06 17:08:05 -0300

    feat: outputting dynamic content with pug

--- app.js
+++ app.js
@@ -2,19 +2,21 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 
-const adminRoutes = require('./routes/admin')
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const port = 3000
-
 const app = express()
+
+app.set('view engine', 'pug')
+app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/favicon.ico', (req, res) => res.status(204));
 
-app.use('/admin', adminRoutes)
+app.use('/admin', adminData.routes)
 
 app.use(shopRoutes)
 
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const path = require('path')
 const rootDir = require('../utils/path')
+const adminData = require('./admin')
 
 
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+  const products = adminData.products
+  res.render('shop', {prods: products, docTitle: 'Shop'})
 })
 
 module.exports = router
