33266493eb1d3fa11e127bc0b04d00eff11ef24f
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-07-18 01:13:31 -0300

    feat:linking the edit page and populating with data with query params

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,10 @@
 const Product = require('../models/product')
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', 
+  res.render('admin/edit-product', 
   {pageTitle: 'Add Product',
   path: '/admin/add-product',
-  formsCSS: true,
-  productCSS: true,
-  activeAddProduct: true
+  editing: false
 })
 }
 
@@ -16,6 +14,27 @@ exports.postAddProduct = (req, res, next) => {
   product.save()
   console.log(product)
   res.redirect('/')
+}
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit
+  if(!editMode) {
+    return res.redirect('/')
+  }
+  const prodId = req.params.productId
+  console.log(prodId)
+  Product.findById(prodId, (product) => {
+    if(!product) {
+      return res.redirect('/')
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product,
+    });
+  });
+  
 }
 
 exports.getProducts = (req, res, next) => {
