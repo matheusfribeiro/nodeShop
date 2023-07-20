f716cec2d4d229c0a048ae6a460a0369effdf284
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-07-20 01:41:43 -0300

    feat:displaying cart items and deleting

--- controllers/shop.js
+++ controllers/shop.js
@@ -34,12 +34,29 @@ exports.getIndex = (req, res, next) => {
   });
 }
 
+
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
-  })
-}
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          prod => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts
+      });
+    });
+  });
+};
+
+
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId
@@ -47,6 +64,14 @@ exports.postCart = (req, res, next) => {
     Cart.addProduct(prodId, product.price)
   }))
   res.redirect('/cart')
+}
+
+exports.postCarDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price)
+    res.redirect('/cart')
+  })
 }
 
 exports.getOrders = (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -15,6 +15,8 @@ router.get('/cart', shopController.getCart)
 
 router.post('/cart', shopController.postCart)
 
+router.post('/cart-delete-item', shopController.postCarDeleteProduct)
+
 router.get('/orders', shopController.getOrders)
 
 router.get('/checkout', shopController.getCheckout)
